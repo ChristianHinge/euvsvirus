94d6b6224dd2c3033738df4ef94bcd8edb58e1b9
HallgrimurHrafn <halligrims@gmail.com>
2020-04-24 22:29:07 +0200

    web tests are fun

--- website/views.py
+++ website/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import  HttpResponse
+import os
+
+cwd = os.getcwd()
+print(cwd)
+
 
 
 
 
 def index(request):
-    return render(request,'website/index.html')
+    return render(request,'index.html')
