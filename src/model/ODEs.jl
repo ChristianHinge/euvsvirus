f6d9fbc062acc7c875ba728d00022383851f0437
Christian <s134801@student.dtu.dk>
2020-04-24 18:36:18 +0200

    SEIR init

--- src/model/ODEs.jl
+++ src/model/ODEs.jl
@@ -1,10 +1,26 @@
 #!/usr/bin/env julia
 
 module ODEs
-using DifferentialEquations: ODEProblem, solve, ODESolution, CallbackSet
-using DiffEqCallbacks: TerminateSteadyState, PositiveDomain
+using DifferentialEquations: ODEProblem, solve, ODESolution
 
 
+
+function SEIR!(du, u, t, β, γ, σ)
+    S = @view u[:,1]
+    E = @view u[:,2]
+    I = @view u[:,3]
+    R = @view u[:,4]
+    N = S .+ E .+ I .+ R
+    du[:,1] .= dSdt(S, I, N, β)
+    du[:,2] .= dEdt(S, E, I, N, β, σ)
+    du[:,3] .= dIdt(E, I, γ, σ)
+    du[:,4] .= dRdt(I, γ)
+end
+
+dSdt(S, I, N, β) = - β .* S .* I ./ N
+dEdt(S, E, I, N, β, σ) = β .* S .* I ./ N .- σ .* E
+dIdt(E, I, γ, σ) = σ .* E .- γ .* I
+dRdt(I, γ) = γ .* I
 
 
 
