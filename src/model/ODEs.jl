f4cda95761c5dd5bf466b672735aaddb8999d778
Christian <s134801@student.dtu.dk>
2020-04-24 18:07:17 +0200

    here there will be ODEs to simulate and run

---
+++ src/model/ODEs.jl
@@ -0,0 +1,11 @@
+#!/usr/bin/env julia
+
+module ODEs
+using DifferentialEquations: ODEProblem, solve, ODESolution, CallbackSet
+using DiffEqCallbacks: TerminateSteadyState, PositiveDomain
+
+
+
+
+
+end;
