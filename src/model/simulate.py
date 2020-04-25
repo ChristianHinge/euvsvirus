2775d595d9e19ae49eae92512f1cbfb39caeb730
Christian <s134801@student.dtu.dk>
2020-04-25 20:58:55 +0200

    A,D,ICU added to simulation

--- src/model/simulate.py
+++ src/model/simulate.py
@@ -1,14 +1,57 @@
 #!/usr/bin/env python3
+import numpy as np
 import pandas as pd
 from src.model.simulation.ODEs import simulate_SEIR_betas
 
-populations = pd.read_csv("data/counties/population/density.tsv", sep="\t")
-fips2pop = dict(zip(populations["fips"], populations["population"]))
+# fractions of removed that die as a function of age group [<18, otherwise, >=65]
+# numbers from Hinge.
+removed_fatality = np.asarray([0.103181947, 0.842995744, 8.199770342]) / 100
 
-def simulate_county(fips, duration, I0=1, intervals=None, interval_beta_factors=None):
+# read
+populations = pd.read_csv("data/counties/population/density.tsv", sep="\t")
+age_groups = pd.read_csv("data/counties/county_health_rankings/county_age.tsv", sep="\t")
+betas = pd.read_csv("data/counties/betas.tsv", sep="\t")
+fips2pop = dict(zip(populations["fips"], populations["population"]))
+fips2beta = dict(zip(betas["fips"], betas["beta"]))
+fips2age = dict(zip(age_groups["fips"], zip(age_groups["percent_less_than_18_years_of_age"], age_groups["percent_65_and_over"])))
+
+
+def _simulate_county(fips, duration, I0=1, intervals=None, interval_beta_factors=None):
+    """
+    Simulate S,E,I,R,t from a county.
+    :param fips: 
+    :param duration: 
+    :param I0: 
+    :param intervals: 
+    :param interval_beta_factors: 
+    :return: 
+    """
     N = fips2pop[fips]
+    beta = fips2beta[fips]
     arr = simulate_SEIR_betas(duration, N-I0, I0=I0, beta=beta, intervals=intervals, interval_beta_factors=interval_beta_factors)
     arr = pd.DataFrame(arr, columns=["S", "E", "I", "R"])
     arr["t"] = arr.index
     return arr
 
+
+def simulate_county(fips, duration, I0=1, intervals=None, interval_beta_factors=None):
+    """
+    Simulate S,E,I,R,t,A,D,ICU from a county 
+    :param fips: 
+    :param duration: 
+    :param I0: 
+    :param intervals: 
+    :param interval_beta_factors: 
+    :return: 
+    """
+    arr = _simulate_county(fips, duration, I0=I0, intervals=intervals, interval_beta_factors=interval_beta_factors)
+    ages = fips2age[fips]
+    ages = np.asarray(ages[0], 100 - sum(ages), ages[1]) / 100
+    arr["D"] = sum(removed_fatality * ages) * arr["R"]
+    arr["A"] = arr["R"] - arr["D"]
+    daily_dead = np.diff(arr["D"])
+    arr["ICU"] = 0
+    for ICU_day in range(5):
+        arr.loc[:-ICU_day-1, "ICU"] += daily_dead[ICU_day:]
+    return arr
+        
--- src/model/simulation/ODEs.py
+++ src/model/simulation/ODEs.py
@@ -5,11 +5,11 @@ from scipy.integrate import odeint
 
 def SEIR(y, t, beta, gamma, sigma):
     """
-    @param y: [S, E, I, R]
-    @param t: time
-    @param beta: rate of S->E
-    @param gamma: rate of I->R
-    @param sigma: rate of E->I
+    :param y: [S, E, I, R]
+    :param t: time
+    :param beta: rate of S->E
+    :param gamma: rate of I->R
+    :param sigma: rate of E->I
     """
     S, E, I, R = y
     N = S + E + I + R
@@ -24,13 +24,13 @@ def SEIR(y, t, beta, gamma, sigma):
 
 def SEIR_betas(y, t, beta, gamma, sigma, intervals, interval_betas):
     """
-    @param y: [S, E, I, R]
-    @param t: time
-    @param beta: default beta outside specified intervals
-    @param gamma: rate of I->R
-    @param sigma: rate of E->I
-    @param intervals: inclusive intervals, e.g. [[start0,end0],[start1,end1],...]
-    @param interval_betas: beta value for each of the intervals
+    :param y: [S, E, I, R]
+    :param t: time
+    :param beta: default beta outside specified intervals
+    :param gamma: rate of I->R
+    :param sigma: rate of E->I
+    :param intervals: inclusive intervals, e.g. [[start0,end0],[start1,end1],...]
+    :param interval_betas: beta value for each of the intervals
     """
     for interval, interval_beta in zip(intervals, interval_betas):
         if interval[0] <= t <= interval[1]: return SEIR(y, t, interval_beta, gamma, sigma)
@@ -45,7 +45,7 @@ def simulate_SEIR(duration, S0, E0=0, I0=1, R0=0, beta=None, gamma=1/14, sigma=1
 
 def simulate_SEIR_betas(duration, S0, E0=0, I0=1, R0=0, beta=None, gamma=1/14, sigma=1/2, intervals=None, interval_beta_factors=None):
     """
-    @param interval_beta_factors: factor on default beta within each intervals, e.g. [2, 1/2] for a panic buying interval then a lockdown interval 
+    :param interval_beta_factors: factor on default beta within each intervals, e.g. [2, 1/2] for a panic buying interval then a lockdown interval 
     """
     assert beta is not None
     if intervals is None: intervals = []
