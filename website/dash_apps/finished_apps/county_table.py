1b3053ab1309f76349778317812e3179afcd9234
ChristianHinge <christian@hingepedersen.dk>
2020-04-25 23:00:08 +0200

    table 2

--- website/dash_apps/finished_apps/county_table.py
+++ website/dash_apps/finished_apps/county_table.py
@@ -1,29 +1,60 @@
 import pandas as pd
+from matplotlib import pyplot as plt
+import numpy as np
+import plotly.graph_objs as go
 
 fips = 1017
 
-df = pd.DataFrame.from_csv('data/counties/betas.tsv', sep='\t')
-county,state = df.iloc[df.index == fips].location.item().split(", ")
+df = pd.DataFrame.from_csv('data/counties/population/density.tsv', sep='\t')
+df_health = pd.DataFrame.from_csv('data/counties/county_health_rankings/county_age.tsv', sep='\t')
 
 
-population =3
-p_65 = 4
+
+county_data = df.loc[df.fips == fips]
+county_health_data = df_health.loc[df_health.index == fips]
+
+population = county_data.population.item()
+county, state = county_data.location.item().split(', ')
+
+age = county_health_data["percent_65_and_over"].item()
+plt.hist(np.log(df["density"].to_numpy()))
+
+
+demographic = ""
+if age < 16.2:
+    demographic = "Young"
+elif age < 18.8:
+    demographic = "Normal"
+elif age < 21.7:
+    demographic = "Old"
+else:
+    demographic = "Very old"
+
+logdens = np.log(county_data["density"].item())
+
+if logdens < 2:
+    density = "Low"
+elif logdens < 6:
+    density = "Normal"
+else:
+    density = "High"
+
+
+p_65 = demographic
 ICU_beds = 5 
-density = 6
 ensured = 7
 risk = 8
 
 
-
-"""
 fig = go.Figure(data=[go.Table(
     header=dict(values=['Property', 'Value'],
                 line_color='darkslategray',
                 fill_color='lightskyblue',
                 align='left'),
-    cells=dict(values=[["State","County","Population","Fraction > 65 yr.", "ICU beds", "Density","Ensured","Risk"], # 1st column
-                    [95, 85, 75, 95]], # 2nd column
+    cells=dict(values=[["State","County","Population","Age demographic", "ICU beds", "Pop. density","Ensured","Risk"], # 1st column
+                    [state, county, population, demographic,2,density,2,2]], # 2nd column
             line_color='darkslategray',
             fill_color='lightcyan',
-            align='left')))
-"""
+            align='left'))
+    ])
+fig.show()
