e9fd8679cb2d09f014872713f7e5f6aa9ccc9e41
Christian <s134801@student.dtu.dk>
2020-04-25 23:19:27 +0200

    print more things for each county

--- data/counties/simulations/simulate_all_counties.py
+++ data/counties/simulations/simulate_all_counties.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
-import numpy as np
+import pandas as pd
 from src.model.simulate import simulate_county
 
-fips = np.loadtxt("data/counties/fips.txt", dtype=int)
+density = pd.read_csv("data/counties/population/density.tsv", sep="\t")
 
-for _fips in fips:
-    arr = simulate_county(_fips, 500)
-    print("{}\t{}".format(_fips, max(arr["ICU"]) / arr["icu_beds"][0]))
+print("fips\tmax_icu\tmax_icu/icu_beds\tD\tD/pop")
+for fips, pop in zip(density["fips"], density["population"]):
+    arr = simulate_county(fips, 500)
+    max_icu = max(arr["ICU"])
+    icu_beds = arr.at[0, "icu_beds"]
+    D = arr.at[-1, "D"]
+    print("\t".join(map(str, (fips, max_icu, max_icu/icu_beds, D, D/pop))))
