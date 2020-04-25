97abcb2936af98e8521cbfaa0b0f1b8d3547d5b0
Christian <s134801@student.dtu.dk>
2020-04-25 16:39:16 +0200

    read_csv instead of read_table

--- src/model/simulate.py
+++ src/model/simulate.py
@@ -2,9 +2,9 @@
 import pandas as pd
 from src.model.simulation.ODEs import simulate_SEIR_betas
 
-populations = pd.read_table("data/counties/population.tsv", sep="\t")
-# betas = pd.read_table("data/counties/betas.tsv", sep="\t")
-# gammas = pd.read_table("data/counties/gammas.tsv", sep="\t")
+populations = pd.read_csv("data/counties/population.tsv", sep="\t")
+# betas = pd.read_csv("data/counties/betas.tsv", sep="\t")
+# gammas = pd.read_csv("data/counties/gammas.tsv", sep="\t")
 fips2pop = dict(zip(populations["fips"], populations["population"]))
 # fips2beta = dict(zip(betas["fips"], betas["beta"]))
 # fips2gamma = dict(zip(gammas["fips"], gammas["gamma"]))
