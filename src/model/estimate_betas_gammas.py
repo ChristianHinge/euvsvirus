e6c99a0885896ea2000244e1340a671476ee9399
Christian <s134801@student.dtu.dk>
2020-04-25 20:10:27 +0200

    set beta values for each county based on population density

--- src/model/estimate_betas_gammas.py
+++ src/model/estimate_betas_gammas.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
+import numpy as np
 import pandas as pd
+from src.utilities import array_util as ar
 
 county_age = pd.read_csv("data/counties/county_health_rankings/county_age.tsv", sep="\t")
 county_health = pd.read_csv("data/counties/county_health_rankings/county_health_population.tsv", sep="\t")
@@ -11,7 +13,15 @@ county_density = pd.read_csv("data/counties/population/density.tsv", sep="\t")
 # len(county_health["fips"].unique())
 # len(county_uninsured["fips"].unique())
 # len(county_density["fips"].unique())
-fips = county_density["fips"]
+
+# USA r0 is [2.0,2.5] according to Hinge. The extremes of county density is NYC and some random place in alaska.
+# we (log) interpolate r0 from 1 to 3 between these extremes
+logdens = np.log(county_density["density"])
+r0 = ar.lerp(1.5, 3.0, ar.lerp_inverse(min(logdens), max(logdens), logdens))
+# r0 == beta / gamma => beta = r0 * gamma
+beta = r0 / 14
+county_betas = pd.DataFrame()
+
 
 
 
