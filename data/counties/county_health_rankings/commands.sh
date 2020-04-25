76f6a4f150ed91c90e57bd5f83e44dac099903eb
Christian <s134801@student.dtu.dk>
2020-04-25 15:20:55 +0200

    population numbers for each county

--- data/counties/county_health_rankings/commands.sh
+++ data/counties/county_health_rankings/commands.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env zsh
 cut -f1,5 < county_health_population.tsv | tr -d '\r' | sed 1d | sed $'s/\t/: /' | sed 's/$/,/' | cat <(echo '{') - <(echo '}') > county_heath.json
+cut -f-4 < county_health_population.tsv > ../population.tsv
