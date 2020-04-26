0b31e12ca0f7c27f1117543bd35ac79474b5d167
Christian <s134801@student.dtu.dk>
2020-04-26 12:37:37 +0200

    risk index and other measures in a single .csv file with 0 prepended fips

--- data/counties/simulations/simulate_all_counties.py
+++ data/counties/simulations/simulate_all_counties.py
@@ -1,18 +1,13 @@
 #!/usr/bin/env python3
 import pandas as pd
 from src.model.simulate import simulate_county
-from matplotlib import pyplot as plt
 
 density = pd.read_csv("data/counties/population/density.tsv", sep="\t")
 
-arr = simulate_county(1043, 500, lockdown=[30, 200])
-plt.plot(arr["Days"], arr["Infected"])
-plt.plot(arr["Days"], arr["Susceptible"])
-
-print("fips\tmax_icu\tmax_icu/icu_beds\tDead\tDead/pop")
+print("fips\tPeak ICU\tPeak ICU/ICU beds\tFatalities\tFatalities/population")
 for fips, pop in zip(density["fips"], density["population"]):
     arr = simulate_county(fips, 500)
     max_icu = max(arr["ICU"])
-    icu_beds = arr.at[0, "icu_beds"]
-    D = arr.at[len(arr)-1, "Dead"]
-    print("\t".join(map(str, (fips, max_icu, max_icu/icu_beds, D, D/pop))))
+    icu_beds = arr.at[0, "ICU beds"]
+    dead = arr.at[len(arr)-1, "Dead"]
+    print("\t".join(map(str, (fips, max_icu, max_icu/icu_beds, dead, dead/pop))))
