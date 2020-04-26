b567594dbc433924433bd91dbb11c6c3109f5f8c
Christian <s134801@student.dtu.dk>
2020-04-26 04:56:44 +0200

    confused recovered and removed

--- src/model/simulate.py
+++ src/model/simulate.py
@@ -83,8 +83,8 @@ def _add_AD(arr, fips):
     """
     ages = fips2age[fips]
     ages = np.asarray([ages[0], 100 - sum(ages), ages[1]]) / 100
-    arr["Dead"] = sum(removed_fatality * ages) * arr["Recovered"]
-    arr["Recovered"] = arr["Recovered"] - arr["Dead"]
+    arr["Dead"] = sum(removed_fatality * ages) * arr["Removed"]
+    arr["Recovered"] = arr["Removed"] - arr["Dead"]
     return arr
 
 
